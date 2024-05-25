bca99dae2133deb59bfe3a9d8b5b409aae93ad64
%Majeed800% <%email%>
2024-05-25 16:05:40 -0400

    Department class completed

---
+++ FinalProject/src/main/java/org/example/Department.java
@@ -0,0 +1,47 @@
+package org.example;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
+public class Department {
+
+    private String departmentId;
+    private String departmentName;
+    private static int nextId = 1;
+
+    public Department(String departmentName) {
+        if (validateDepartmentName(departmentName)) {
+            this.departmentId = "D" + String.format("%02d", nextId++);
+            this.departmentName = departmentName;
+        } else {
+            this.departmentId = null;
+            this.departmentName = null;
+        }
+    }
+
+    /**
+     * checks if a department name is valid or not, a department name should only contain letters or space
+     * @param departmentName
+     * @return true if valid, else false
+     */
+    public static boolean validateDepartmentName(String departmentName) {
+        if (departmentName == null || departmentName.isEmpty()) {
+            return false;
+        }
+
+        for (int i = 0; i < departmentName.length(); i++) {
+            char c = departmentName.charAt(i);
+            if (!Character.isLetter(c) && c != ' ') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
--- FinalProject/src/main/java/org/example/Main.java
+++ FinalProject/src/main/java/org/example/Main.java
@@ -4,7 +4,6 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Address.isPostalCodeValid("H3N 2S1" +
-                ""));
+
     }
 }
